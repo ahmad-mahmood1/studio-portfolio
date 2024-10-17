fcfac00018345476cf3960664adfc8f3b8cae08b
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2024-10-17 21:33:08 +0500

    project skills

--- sanity/schemaTypes/project.ts
+++ sanity/schemaTypes/project.ts
@@ -1,42 +1,54 @@
-import { defineField, defineType } from "sanity";
+import {defineField, defineType} from 'sanity'
 
 export const projectType = defineType({
-  name: "project",
-  title: "Projects",
-  type: "document",
+  name: 'project',
+  title: 'Projects',
+  type: 'document',
   fields: [
     defineField({
-      name: "title",
-      type: "string",
+      name: 'title',
+      type: 'string',
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: "slug",
-      type: "slug",
-      options: { source: "title" },
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: "publishedAt",
-      type: "datetime",
+      name: 'publishedAt',
+      type: 'datetime',
       initialValue: () => new Date().toISOString(),
+    }),
+    defineField({
+      name: 'images',
+      type: 'array',
+      of: [{type: 'image'}],
+    }),
+    defineField({
+      name: 'description',
+      type: 'text',
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: "images",
-      type: "array",
-      of: [{ type: "image" }],
+      name: 'role',
+      type: 'array',
+      of: [{type: 'block'}],
+      validation: (rule) => rule.required(),
     }),
+
     defineField({
-      name: "description",
-      type: "text",
+      name: 'projectUrl',
+      title: 'Project Url',
+      type: 'url',
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: "projectUrl",
-      title: "Project Url",
-      type: "url",
-      validation: (rule) => rule.required(),
+      title: 'Skills',
+      name: 'skills',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'skill'}]}],
     }),
   ],
-});
+})
